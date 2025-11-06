f704c09c5a937f7bee8b0ec6c646c432f9ddadc7
emman tenorio <204869950+laflamee15@users.noreply.github.com>
2025-11-06 09:44:15 +0800

    Delete pracPYTHON directory

--- pracPYTHON/find_largest.py
+++
@@ -1,7 +0,0 @@
-def find_largest(a, b, c):
-    result = max(a, b, c)
-    print(f"Largest number: {result}")
-first = int(input("Enter first number: "))
-second = int(input("Enter second number: "))
-third = int(input("Enter third number: "))
-find_largest(first, second, third)
--- pracPYTHON/greenbow/evenAverage.py
+++
@@ -1,9 +0,0 @@
-numbers = list(map(int, input("Enter number: ").split()))
-evenNumbers = [num for num in numbers if num %2 == 0]
-if evenNumbers:
-    average = (sum(evenNumbers) / len(evenNumbers))
-    print(f"Even numbers: {evenNumbers}")
-    print(f"Average of even: {round(average, 2)}")
-else:
-    print("No even numbers")
-
